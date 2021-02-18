c9a6c2e04b1a154b46bd9837d69c1c062b511f12
val <valentin.droubay@edu.ece.fr>
2021-02-18 17:34:53 +0000

    Cmakelist

---
+++ cmake-build-debug/CMakeFiles/ClashOfECEprojet.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ClashOfECEprojet.dir/Main.cpp.obj"
+  "ClashOfECEprojet.exe"
+  "ClashOfECEprojet.exe.manifest"
+  "ClashOfECEprojet.pdb"
+  "libClashOfECEprojet.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ClashOfECEprojet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
